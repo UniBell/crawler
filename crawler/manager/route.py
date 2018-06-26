709448a1719c84d01bf7947f4fa6182d35a19a12
raven <zhangxy@knowbox.cn>
2018-06-26 20:26:15 +0800

    test

--- crawler/manager/route.py
+++ crawler/manager/route.py
@@ -6,3 +6,9 @@ from crawler.manager import app
 @app.route('/')
 def hello_world():
     return 'Hello, World 123!'
+
+@app.route('/api/project')
+def get_data():
+    return {
+        'name': '123'
+    }
