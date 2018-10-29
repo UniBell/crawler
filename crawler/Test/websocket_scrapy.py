696e0180fed398ef58b4f1662218e062c4d96770
raven <xyzhang1123@gmail.com>
2018-10-29 16:38:36 +0800

    格式修改

--- crawler/Test/websocket_scrapy.py
+++ crawler/Test/websocket_scrapy.py
@@ -1,15 +1,14 @@
 # from ws4py.client.threadedclient import WebSocketClient
 import asyncio
 import websockets
-import json
+# import json
 
 async def bitmex():
     async with websockets.connect(
-        'wss://www.bitmex.com/realtime'
-    ) as websocket:
-    param = {"op": "subscribe", "args": ["orderBookL2:XBTUSD"]}
-    await websocket.send(param)
-    resp = await websocket.recv()
-    print(resp)
+        'wss://www.bitmex.com/realtime') as websocket:
+        param = {"op": "subscribe", "args": ["orderBookL2:XBTUSD"]}
+        await websocket.send(param)
+        resp = await websocket.recv()
+        print(resp)
 
 asyncio.get_event_loop().run_until_complete(bitmex())
