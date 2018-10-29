96ac77842217eb0db323ac1472a8c0f9faf17124
raven <xyzhang1123@gmail.com>
2018-10-29 16:51:50 +0800

    save

--- crawler/Test/websocket_scrapy.py
+++ crawler/Test/websocket_scrapy.py
@@ -3,7 +3,7 @@ import asyncio
 import websockets
 # import json
 
-async def bitmex():
+async def bitmex( ):
     async with websockets.connect(
         'wss://www.bitmex.com/realtime') as websocket:
         param = {"op": "subscribe", "args": ["orderBookL2:XBTUSD"]}
