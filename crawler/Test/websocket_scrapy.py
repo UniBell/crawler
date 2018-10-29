c74de19bdea0303cb67f1fdd5f827f8ec4f2556c
raven <xyzhang1123@gmail.com>
2018-10-29 19:18:41 +0800

    save

--- crawler/Test/websocket_scrapy.py
+++ crawler/Test/websocket_scrapy.py
@@ -1,39 +1,44 @@
-from ws4py.client.threadedclient import WebSocketClient
-# import asyncio
-# import websockets
+# from ws4py.client.threadedclient import WebSocketClient
+import asyncio
+import websockets
 import json
 
-# async def bitmex():
-#     async with websockets.connect(
-#         'wss://www.bitmex.com/realtime') as websocket:
-#         param = {"op": "subscribe", "args": ["orderBookL2:XBTUSD"]}
-#         await websocket.send(json.dumps(param))
-#         resp = await websocket.recv()
-#         print(resp)            
+@asyncio.coroutine
+def bitmex():
+    websocket = yield from websockets.connect(
+        'wss://www.bitmex.com/realtime')
+    try:
+        param = {"op": "subscribe", "args": ["orderBookL2:XBTUSD"]}
+        yield from websocket.send(json.dumps(param))
+        resp = yield from websocket.recv()
+        print(resp)    
+    finally:
+        yield from websocket.close()        
 
-# asyncio.get_event_loop().run_until_complete(bitmex())
-# asyncio.get_event_loop().run_forever()
+asyncio.get_event_loop().run_until_complete(bitmex())
 
-class BT_Client(WebSocketClient):
+
+#旧写法
+# class BT_Client(WebSocketClient):
    
-   def opened(self):
-       req = '{"op":"subscribe", "args": ["orderBookL2:XBTUSD"]}'
-       self.send(req)
+#    def opened(self):
+#        req = '{"op":"subscribe", "args": ["orderBookL2:XBTUSD"]}'
+#        self.send(req)
    
-   def closed(self, code, reason=None):
-       print("Closed down:", code, reason)
+#    def closed(self, code, reason=None):
+#        print("Closed down:", code, reason)
    
-   def received_message(self, resp):
-       resp = json.loads(str(resp))
-       # data = resp['data']
-       print(resp)
+#    def received_message(self, resp):
+#        resp = json.loads(str(resp))
+#        # data = resp['data']
+#        print(resp)
 
 
-if __name__ == '__main__':
-   ws = None
-   try:
-       ws = BT_Client('wss://www.bitmex.com/realtime')
-       ws.connect()
-       ws.run_forever()
-   except KeyboardInterrupt:
-       ws.close()
+# if __name__ == '__main__':
+#    ws = None
+#    try:
+#        ws = BT_Client('wss://www.bitmex.com/realtime')
+#        ws.connect()
+#        ws.run_forever()
+#    except KeyboardInterrupt:
+#        ws.close()
