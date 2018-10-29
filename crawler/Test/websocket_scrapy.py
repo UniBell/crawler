637da7cea6f7402c8f9f4ec3f2054f6fca0bd3e1
raven <xyzhang1123@gmail.com>
2018-10-29 16:35:16 +0800

    修改bitmex websocket写法

--- crawler/Test/websocket_scrapy.py
+++ crawler/Test/websocket_scrapy.py
@@ -1,26 +1,15 @@
-from ws4py.client.threadedclient import WebSocketClient
+# from ws4py.client.threadedclient import WebSocketClient
+import asyncio
+import websockets
 import json
 
-class BT_Client(WebSocketClient):
-    
-    def opened(self):
-        req = '{"op":"subscribe", "args": ["orderBookL2:XBTUSD"]}'
-        self.send(req)
+async def bitmex():
+    async with websockets.connect(
+        'wss://www.bitmex.com/realtime'
+    ) as websocket:
+    param = {"op": "subscribe", "args": ["orderBookL2:XBTUSD"]}
+    await websocket.send(param)
+    resp = await websocket.recv()
+    print(resp)
 
-    def closed(self, code, reason=None):
-        print("Closed down:", code, reason)
-
-    def received_message(self, resp):
-        resp = json.loads(str(resp))
-        # data = resp['data']
-        print(resp)
-
-
-if __name__ == '__main__':
-    ws = None
-    try:
-        ws = BT_Client('wss://www.bitmex.com/realtime')
-        ws.connect()
-        ws.run_forever()
-    except KeyboardInterrupt:
-        ws.close()
+asyncio.get_event_loop().run_until_complete(bitmex())
